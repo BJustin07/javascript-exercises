82dfba4522b37f7ce93666026fe50a04dbe1b893
BJustin07 <baquiranjustin07@gmail.com>
2024-07-29 13:32:38 +0800

    Completed:jsExercise03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let setString = string;
+    let stringResult = "";
+    for(let i = 0; i < num; i ++){
+        stringResult = stringResult.concat(setString); 
+    }
+    return stringResult;
 };
-
+repeatString("hey", 5);
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(sentence) {
+    let revSentence = sentence.split("").reverse().join("");
+    return revSentence;
 };
+reverseString("pancit canton");
 
 // Do not edit below this line
 module.exports = reverseString;
