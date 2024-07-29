c114e44de70918fea8d58544b7032a363a9f9d1e
BJustin07 <baquiranjustin07@gmail.com>
2024-07-29 14:02:46 +0800

    Comleted:jsExercise04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, valueToRemove) {
+    return array.filter(element => element !== valueToRemove);
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
